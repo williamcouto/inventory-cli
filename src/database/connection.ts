c479a590275d564d1e8412c240999df52921f296
williamcouto <willplacido40@gmail.com>
2026-04-27 22:09:47 -0300

    fix: corrigindo importação do better-sqlite3

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,4 +1,4 @@
-import sqlite3 from 'sqlite3'
+import Database from 'better-sqlite3'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
@@ -7,12 +7,5 @@ const __dirname = path.dirname(__filename)
 
 // dirname apontará para o caminho src/database
 const pathDatabase = path.resolve(__dirname, '..', '..', 'data', 'inventory.sqlite')
-const dbInventory = new sqlite3.Database(pathDatabase, (error) => {
-    if(error){
-        console.log('Erro na conexão com o banco de dados')
-    }
-    else{
-        console.log("Conexão estabelecida")
-    }
-})
+const dbInventory = new Database(pathDatabase)
 export default dbInventory
