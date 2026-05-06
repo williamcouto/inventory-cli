eeca6c02d210553e09ccbe044e8c5bf529ac7a08
williamcouto <willplacido40@gmail.com>
2026-05-05 22:18:53 -0300

    chore: Inserindo as validações de preço e quantidade

--- src/services/product-service.ts
+++ src/services/product-service.ts
@@ -6,12 +6,23 @@ export class ProductService{
     private repo = new ProductRepo()
     
     addProduct(product: Produto): void{
-        if(!product.name){
+        if(!product.name.trim()){
             throw new Error("O nome do produto é obrigatório")
         }
         if(product.quantity < 0){
             throw new Error("Quantidade não pode ser negativa")
         }
+
+        if(product.price == undefined){
+            throw new Error("O preço do produto é obrigatório")
+        }
+        if(product.price < 0){
+            throw new Error("O valor não pode ser negativo")
+        }
         this.repo.saveProduct(product)
+    }
+
+    listProducts(){
+        this.repo.listAllProducts()
     }
 }
